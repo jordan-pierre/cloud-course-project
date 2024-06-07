4b79ca3738329a9ecd308dba4b97a00846c3747e
jordan <jpierre43@gatech.edu>
2024-06-07 18:36:55 -0400

    Added a mocked_aws test fixture

--- tests/unit_tests/s3/test__write_objects.py
+++ tests/unit_tests/s3/test__write_objects.py
@@ -4,25 +4,11 @@ import boto3
 from moto import mock_aws
 
 from files_api.s3.write_objects import upload_s3_object
-
-TEST_BUCKET_NAME = "test-bucket-mlops-club-a2fl"
-
-
-def point_away_from_aws():
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+from tests.consts import TEST_BUCKET_NAME
 
 
 @mock_aws
-def test__upload_s3_object():
-    point_away_from_aws()
-
-    # Create a S3 bucket
-    s3_client = boto3.client("s3")
-    s3_client.create_bucket(Bucket=TEST_BUCKET_NAME)
+def test__upload_s3_object(mocked_aws):
 
     # Upload a file to the bucket with a particular content type
     object_key = "test.txt"
@@ -34,18 +20,10 @@ def test__upload_s3_object():
         object_key=object_key,
         file_content=file_content,
         content_type=content_type,
-        s3_client=s3_client,
     )
 
     # Check that the file was uploaded with a particular content type
+    s3_client = boto3.client("s3")
     response = s3_client.get_object(Bucket=TEST_BUCKET_NAME, Key=object_key)
     assert response["ContentType"] == content_type
     assert response["Body"].read() == file_content
-
-    # Clean up by deleting the files and bucket
-    response = s3_client.list_objects_v2(Bucket=TEST_BUCKET_NAME)
-
-    for obj in response.get("Contents", []):
-        s3_client.delete_object(Bucket=TEST_BUCKET_NAME, Key=obj["Key"])
-
-    s3_client.delete_bucket(Bucket=TEST_BUCKET_NAME)
