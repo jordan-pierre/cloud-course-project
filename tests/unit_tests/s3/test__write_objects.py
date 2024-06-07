2e1016a9756b72e5cb0cb307596b2c94670458cb
jordan <jpierre43@gatech.edu>
2024-06-07 17:55:28 -0400

    Added a unit test and .vscode settings to run tests with environment variable

---
+++ tests/unit_tests/s3/test__write_objects.py
@@ -0,0 +1,37 @@
+import boto3
+
+from files_api.s3.write_objects import upload_s3_object
+
+TEST_BUCKET_NAME = "test-bucket-mlops-club-a2fl"
+
+
+def test__upload_s3_object():
+    # Create a S3 bucket
+    s3_client = boto3.client("s3")
+    s3_client.create_bucket(Bucket=TEST_BUCKET_NAME)
+
+    # Upload a file to the bucket with a particular content type
+    object_key = "test.txt"
+    file_content = b"Hello, world!"
+    content_type = "text/plain"
+
+    upload_s3_object(
+        bucket_name=TEST_BUCKET_NAME,
+        object_key=object_key,
+        file_content=file_content,
+        content_type=content_type,
+        s3_client=s3_client,
+    )
+
+    # Check that the file was uploaded with a particular content type
+    response = s3_client.get_object(Bucket=TEST_BUCKET_NAME, Key=object_key)
+    assert response["ContentType"] == content_type
+    assert response["Body"].read() == file_content
+
+    # Clean up by deleting the files and bucket
+    response = s3_client.list_objects_v2(Bucket=TEST_BUCKET_NAME)
+
+    for obj in response.get("Contents", []):
+        s3_client.delete_object(Bucket=TEST_BUCKET_NAME, Key=obj["Key"])
+
+    s3_client.delete_bucket(Bucket=TEST_BUCKET_NAME)
